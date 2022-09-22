6ac213dc506a9bf96b6f9b98615ee2e6938d0183
Koki Seo <kokihfc21@gmail.com>
2022-09-22 07:49:53 +0000

    component作成

---
+++ test/components/list/item_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class List::ItemComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(List::ItemComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
